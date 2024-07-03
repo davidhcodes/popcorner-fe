2d94abdfe6b90267a21a42da9a8d3651283dd7ba
elenamurgia <145214693+elenamurgia@users.noreply.github.com>
2024-07-03 11:59:37 +0100

    creates the homepage, creates the HomeScreen, CommunitiesScreen, ChatsScreen and EventsScreen, updates the _layout.tsx so that the screens are connectet. It also creates api.js in order to connect the movie api. It also creates the PopularMovies.jsx which retrieves the popular movies from the api.

--- popcorner-fe/app/(tabs)/_layout.tsx
+++ popcorner-fe/app/(tabs)/_layout.tsx
@@ -1,59 +1,88 @@
-import React from 'react';
-import FontAwesome from '@expo/vector-icons/FontAwesome';
-import { Link, Tabs } from 'expo-router';
-import { Pressable } from 'react-native';
+import React from "react";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import { useColorScheme } from "@/components/useColorScheme";
+import Colors from "@/constants/Colors";
+import ChatsScreen from "../../screens/ChatsScreen";
+import CommunitiesScreen from "../../screens/CommunitiesScreen";
+import CinemaScreen from "../../screens/CinemaScreen";
+import { MaterialCommunityIcons } from "@expo/vector-icons";
+import HomeScreen from "../../screens/HomeScreen";
+import EventsScreen from "../../screens/EventsScreen";
 
-import Colors from '@/constants/Colors';
-import { useColorScheme } from '@/components/useColorScheme';
-import { useClientOnlyValue } from '@/components/useClientOnlyValue';
-
-// You can explore the built-in icon families and icons on the web at https://icons.expo.fyi/
-function TabBarIcon(props: {
-  name: React.ComponentProps<typeof FontAwesome>['name'];
-  color: string;
-}) {
-  return <FontAwesome size={28} style={{ marginBottom: -3 }} {...props} />;
-}
+const Tab = createBottomTabNavigator();
 
 export default function TabLayout() {
   const colorScheme = useColorScheme();
 
   return (
-    <Tabs
+    <Tab.Navigator
       screenOptions={{
-        tabBarActiveTintColor: Colors[colorScheme ?? 'light'].tint,
-        // Disable the static render of the header on web
-        // to prevent a hydration error in React Navigation v6.
-        headerShown: useClientOnlyValue(false, true),
-      }}>
-      <Tabs.Screen
-        name="index"
+        tabBarActiveTintColor: Colors[colorScheme ?? "light"].tint,
+        headerShown: false,
+      }}
+    >
+      <Tab.Screen
+        name="Home"
+        component={HomeScreen}
         options={{
-          title: 'Tab One',
-          tabBarIcon: ({ color }) => <TabBarIcon name="code" color={color} />,
-          headerRight: () => (
-            <Link href="/modal" asChild>
-              <Pressable>
-                {({ pressed }) => (
-                  <FontAwesome
-                    name="info-circle"
-                    size={25}
-                    color={Colors[colorScheme ?? 'light'].text}
-                    style={{ marginRight: 15, opacity: pressed ? 0.5 : 1 }}
-                  />
-                )}
-              </Pressable>
-            </Link>
+          tabBarLabel: "Home",
+          tabBarIcon: ({ size }) => (
+            <MaterialCommunityIcons name="home" size={size} color="#141E46" />
           ),
         }}
       />
-      <Tabs.Screen
-        name="two"
+      <Tab.Screen
+        name="Events"
+        component={EventsScreen}
         options={{
-          title: 'Tab Two',
-          tabBarIcon: ({ color }) => <TabBarIcon name="code" color={color} />,
+          tabBarLabel: "Events near me",
+          tabBarIcon: ({ size }) => (
+            <MaterialCommunityIcons
+              name="domino-mask"
+              size={size}
+              color="#AF47D2"
+            />
+          ),
         }}
       />
-    </Tabs>
+      <Tab.Screen
+        name="Communities"
+        component={CommunitiesScreen}
+        options={{
+          tabBarLabel: "Communities",
+          tabBarIcon: ({ size }) => (
+            <MaterialCommunityIcons
+              name="account-group"
+              size={size}
+              color="#FF6969"
+            />
+          ),
+        }}
+      />
+      <Tab.Screen
+        name="EventsNearMe"
+        component={CinemaScreen}
+        options={{
+          tabBarLabel: "Cinemas near me",
+          tabBarIcon: ({ size }) => (
+            <MaterialCommunityIcons
+              name="map-marker"
+              size={size}
+              color="#141E46"
+            />
+          ),
+        }}
+      />
+      <Tab.Screen
+        name="Chats"
+        component={ChatsScreen}
+        options={{
+          tabBarLabel: "Chats",
+          tabBarIcon: ({ size }) => (
+            <MaterialCommunityIcons name="chat" size={size} color="#BB2525" />
+          ),
+        }}
+      />
+    </Tab.Navigator>
   );
 }
